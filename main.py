4c3442a1dfa9cac3149d0809dad039ac2c1e0b39
Yasiru Lakshan <yasiru1999@gmail.com>
2022-10-14 13:06:01 +0530

    Code completed

--- main.py
+++ main.py
@@ -34,11 +34,25 @@ file1 = open("a.txt", "r")
 a = readMatrix(file1)
 file1.close()
 if a:
+    rowA = len(a)
+    colomnA = len(a[0])
     file2 = open("b.txt", "r")
     b = readMatrix(file2)
     file2.close()
     if b:
-        print("HY")
+        rowB = len(b)
+        colomnB = len(b[0])
+        if colomnA == colomnB:
+            multi = []
+            for i in range(rowA):
+                row = []
+                for j in range(colomnA):
+                    row.append(a[i][j] * b[i][j])
+                multi.append(row)
+            for r in multi:
+                print(*r)
+        else:
+            print("cannot multiply!!")
     else:
         print("Invalid Matrix B!!")
 else:
